5ce27dd61d1ac5608b7780bbe24702a131757580
Julian Rivas <juliandarivas@hotmail.com>
2019-10-30 23:29:44 -0500

    Add project files.

---
+++ Example.Consola/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Example.Libreria;
+
+namespace Example.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elemento elemento = new Elemento();
+
+            string mensaje = $@"
+                PackageId: {elemento.PackageId}
+                PackageVersion = {elemento.PackageVersion}
+                Author = {elemento.Author}
+                Company = {elemento.Company}
+                Product = {elemento.Product}
+                Description = {elemento.Description}";
+
+            Console.WriteLine(mensaje);
+        }
+    }
+}
---
+++ Example.Libreria/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace Example.Libreria
+{
+    public class Elemento
+    {
+        public string PackageId { get; set; }
+        public string PackageVersion { get; set; }
+        public string Author { get; set; }
+        public string Company { get; set; }
+        public string Product { get; set; }
+        public string Description { get; set; }
+
+        public Elemento()
+        {
+            Assembly ensamblado = Assembly.GetEntryAssembly();
+
+            PackageId = ensamblado?.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+            PackageVersion = ensamblado?.GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+            Company = ensamblado?.GetCustomAttribute<AssemblyCompanyAttribute>().Company;
+            Product = ensamblado?.GetCustomAttribute<AssemblyProductAttribute>().Product;
+            Description = ensamblado?.GetCustomAttribute<AssemblyDescriptionAttribute>().Description;
+        }
+    }
+}
